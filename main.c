601201bf07aff378de9a89a3cfeecbff0647da03
luciodj <pilot@flyingpic24.com>
2019-03-27 18:19:51 +0100

    compiles but reads random

--- main.c
+++ main.c
@@ -33,48 +33,74 @@ SOFTWARE.
 #include "mcc_generated_files/sensors_handling.h"
 #include "mcc_generated_files/cloud/cloud_service.h"
 #include "mcc_generated_files/debug_print.h"
+#include "mcc_generated_files/drivers/i2c_simple_master.h"
+
+// IR Thermo Click(tm) constants
+#define IR_THERMO_ADDRESS   0x5A
+#define AMB_TEMP            0x06
+#define OBJ_TEMP            0x07
+
+//bool IR_SensorRead(uint8_t reg, float * pTemp)
+//{
+//    int16_t data;
+//    I2C2_MESSAGE_STATUS status = I2C2_MESSAGE_PENDING;
+//    static I2C2_TRANSACTION_REQUEST_BLOCK trb[2];
+//
+//    I2C2_MasterWriteTRBBuild(&trb[0], &reg, 1, IR_THERMO_ADDRESS);
+//    I2C2_MasterReadTRBBuild(&trb[1], (uint8_t*)&data, 2, IR_THERMO_ADDRESS);
+//    I2C2_MasterTRBInsert(2, &trb[0], &status);
+//
+//    while(status == I2C2_MESSAGE_PENDING);         // blocking
+//    *pTemp = ((float)(data) *  0.02) - 273.15;     // convert to deg C
+//
+//    return (status == I2C2_MESSAGE_COMPLETE);
+//}
 
 //This handles messages published from the MQTT server when subscribed
 void receivedFromCloud(uint8_t *topic, uint8_t *payload)
 {
     char *toggleToken = "\"toggle\":";
     char *subString;
-    
+
     if ((subString = strstr((char*)payload, toggleToken)))
     {
         LED_holdYellowOn( subString[strlen(toggleToken)] == '1' );
     }
 
-
     debug_printer(SEVERITY_NONE, LEVEL_NORMAL, "topic: %s", topic);
     debug_printer(SEVERITY_NONE, LEVEL_NORMAL, "payload: %s", payload);
 }
 
-// This will get called every 1 second only while we have a valid Cloud connection
+// This will get called every CFG_SEND_INTERVAL only while we have a valid Cloud connection
 void sendToCloud(void)
 {
    static char json[70];
-         
-   // This part runs every CFG_SEND_INTERVAL seconds
-   int rawTemperature = SENSORS_getTempValue();
+
    int light = SENSORS_getLightValue();
-   int len = sprintf(json, "{\"Light\":%d,\"Temp\":\"%d.%02d\"}", light,rawTemperature/100,abs(rawTemperature)%100);
+   int temp = OBJ_TEMP;
+   //   int temp = i2c_read2ByteRegister(IR_THERMO_ADDRESS, OBJ_TEMP);
+   i2c_writeNBytes(IR_THERMO_ADDRESS, &temp, 1);
+   i2c_readNBytes(IR_THERMO_ADDRESS, &temp, 2);
+   printf("temp: %d\n", temp);
+//   temp = ((float)(temp) *  0.02) - 273.15;     // convert to deg C
+
+   int len = sprintf(json, "{\"Light\":%d,\"Temp\":%d}", light, temp);
 
    if (len >0) {
       CLOUD_publishData((uint8_t*)json, len);
       LED_flashYellow();
    }
 }
- 
- 
+
+
 int main(void)
 {
    application_init();
 
    while (1)
-   { 
-      runScheduler();  
+   {
+      runScheduler();
    }
-   
+
    return 0;
 }
